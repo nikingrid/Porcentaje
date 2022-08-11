7fc800820a2d84f11ef7c93efcb32d6852685fd8
nikingrid <nikpam2008@hotmail.com>
2022-08-11 22:20:11 +0200

    Empezando rama de cero

--- src/Start.java
+++ src/Start.java
@@ -5,28 +5,6 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Operaciones op1= new Operaciones ();
-		Operaciones op2= new Operaciones ();
-		Operaciones op3= new Operaciones ();
-		
-		Scanner add=new Scanner(System.in);
-		
-		System.out.print("Intruduzca préstamo inicial: ");
-		op1.setP(add.nextInt());
-		op1.porcentaje();
-		
-		System.out.print("\n" + "\n");
-		
-		System.out.print("¿Cuanto te queda de préstamo?: ");
-		op2.setP(add.nextInt());
-		op2.porcentaje();
-		
-		System.out.print("\n" + "\n");
-		
-		System.out.print("¿Cuanto te queda de préstamo?: ");
-		op3.setP(add.nextInt());
-		op3.porcentaje();
-		
 		
 	}
 
