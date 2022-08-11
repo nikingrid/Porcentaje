3337db13e7c3b4d247db1bbc3709c5df0d0c7572
nikingrid <nikpam2008@hotmail.com>
2022-08-11 22:17:16 +0200

    Add problema resuelto

--- src/Start.java
+++ src/Start.java
@@ -14,6 +14,20 @@ public class Start {
 		System.out.print("Intruduzca préstamo inicial: ");
 		op1.setP(add.nextInt());
 		op1.porcentaje();
+		
+		System.out.print("\n" + "\n");
+		
+		System.out.print("¿Cuanto te queda de préstamo?: ");
+		op2.setP(add.nextInt());
+		op2.porcentaje();
+		
+		System.out.print("\n" + "\n");
+		
+		System.out.print("¿Cuanto te queda de préstamo?: ");
+		op3.setP(add.nextInt());
+		op3.porcentaje();
+		
+		
 	}
 
 }
