8994a829351679bdd77e95337480afb625b03e53
nikingrid <nikpam2008@hotmail.com>
2022-08-11 22:48:40 +0200

    Add commit

--- src/Start.java
+++ src/Start.java
@@ -1,10 +1,19 @@
-import java.util.Scanner;
 
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int i=20000;
+		int r=0;
+		
+		r=i*10/100;
+		i=i-r;
+		
+			if (i!=0) {
+			System.out.println(i);
+			}
+		
 		
 	}
 
