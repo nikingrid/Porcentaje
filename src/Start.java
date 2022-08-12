8dfc60278af046f170ecaa06b3cf17d956c83c1f
nikingrid <nikpam2008@hotmail.com>
2022-08-12 19:35:52 +0200

    Add cambios

--- src/Start.java
+++ src/Start.java
@@ -4,17 +4,27 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int i=20000;
 		int r=0;
 		
-		r=i*10/100;
-		i=i-r;
-		
-			if (i!=0) {
-			System.out.println(i);
+		for (int i=0; i<3; i++) {
+			if (i!=1) {
+				r+=10;
+			} 
+			else {
+				r+=i;
 			}
-		
-		
+			
+		} 
+		System.out.print(r);
+	
+		/* Explain 
+		 * i=0, r+=0 =0
+		 * i=1, r+=1 0+1=1
+		 * i=2, r+=2 1+2=3
+		 * 
+		 * i=3, r+=10 3+10=13
+		 * i=4, r+=4 13+4=17 
+		 */
 	}
 
 }
